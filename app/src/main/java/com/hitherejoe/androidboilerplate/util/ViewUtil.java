1848b4258d7a0f0e4e0a9e33444a5f20f82f1bef
Ivan Carballo <ivan@ribot.co.uk>
2015-04-22 15:14:51 +0100

    Add only gcm play services dependency

--- app/src/main/java/com/hitherejoe/androidboilerplate/util/ViewUtil.java
+++ app/src/main/java/com/hitherejoe/androidboilerplate/util/ViewUtil.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.view.inputmethod.InputMethodManager;
 
-public class ViewUtils {
+public class ViewUtil {
 
     public static float pxToDp(float px) {
         float densityDpi = Resources.getSystem().getDisplayMetrics().densityDpi;
