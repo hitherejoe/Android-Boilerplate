a3c380ab3782095b11eaca03967e8fd20cc78002
Ivan Carballo <ivan@ribot.co.uk>
2015-04-22 14:46:23 +0100

    Add methods to ViewUtils

--- app/src/main/java/com/hitherejoe/androidboilerplate/util/ViewUtils.java
+++ app/src/main/java/com/hitherejoe/androidboilerplate/util/ViewUtils.java
@@ -1,13 +1,25 @@
 package com.hitherejoe.androidboilerplate.util;
 
+import android.app.Activity;
 import android.content.Context;
-import android.util.DisplayMetrics;
+import android.content.res.Resources;
+import android.view.inputmethod.InputMethodManager;
 
 public class ViewUtils {
 
-    public static float convertPixelsToDp(float px, Context context){
-        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        return px / (metrics.densityDpi / 160f);
+    public static float pxToDp(float px) {
+        float densityDpi = Resources.getSystem().getDisplayMetrics().densityDpi;
+        return px / (densityDpi / 160f);
+    }
+
+    public static int dpToPx(int dp) {
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        return Math.round(dp * density);
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(), 0);
     }
 
 }
