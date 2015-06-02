6627d867e408267b6ec40a26adeb84515aec14c3
Jason Fry <jason@jasonfry.co.uk>
2015-06-02 09:36:40 +0100

    Fixed a cheeky bug in the Ribot model

--- app/src/main/java/uk/co/ribot/androidboilerplate/data/model/Ribot.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/data/model/Ribot.java
@@ -24,6 +24,7 @@ public class Ribot implements Parcelable {
         int i = 0;
         for (Ribot ribot : ribots) {
             ids[i] = ribot.id;
+            i++;
         }
         return ids;
     }
