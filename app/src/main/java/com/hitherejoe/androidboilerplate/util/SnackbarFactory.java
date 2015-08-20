d0df0fc6b901b0cc94dd9e831a7490033d3adb85
Joe Birch <joe@ribot.co.uk>
2015-08-20 11:10:55 +0100

    Remove ToastFactory, replace with SnackbarFactory

--- app/src/main/java/com/hitherejoe/androidboilerplate/util/SnackbarFactory.java
+++ app/src/main/java/com/hitherejoe/androidboilerplate/util/SnackbarFactory.java
@@ -1,7 +1,18 @@
 package com.hitherejoe.androidboilerplate.util;
 
-/**
- * Created by hitherejoe on 13/08/15.
- */
+import android.content.Context;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hitherejoe.androidboilerplate.R;
+
 public class SnackbarFactory {
+
+    public static Snackbar createSnackbar(Context context, View view, String message) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT);
+        ViewGroup group = (ViewGroup) snackbar.getView();
+        group.setBackgroundColor(context.getResources().getColor(R.color.primary));
+        return snackbar;
+    }
 }
